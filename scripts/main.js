d5c36c2901e2811f48a8d937ab5437fbf6c57087
Daniel Mundell <mellowdann@gmail.com>
2020-09-02 23:16:18 +0200

    Add files via upload
    Highlighting walls sort of working.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,21 @@
 const draw = SVG().addTo('body').size('100%', '100%')
 
-const Hex = Honeycomb.extendHex({ size: 10 })
+const Hex = Honeycomb.extendHex({ 
+    size: 10,
+    custom: 'empty' })
 const Grid = Honeycomb.defineGrid(Hex)
 // get the corners of a hex (they're the same for all hexes created with the same Hex factory)
-const corners = Hex().corners()
+corners = Hex().corners()
 // an SVG symbol can be reused
-const hexSymbol = draw.symbol()
+const hexEmpty = draw.symbol()
     // map the corners' positions to a string and create a polygon
     .polygon(corners.map(({ x, y }) => `${x},${y}`))
-    .fill('none')
+    .fill('white')
+    .stroke({ width: 1, color: '#999' })
+const hexWall = draw.symbol()
+    // map the corners' positions to a string and create a polygon
+    .polygon(corners.map(({ x, y }) => `${x},${y}`))
+    .fill('black')
     .stroke({ width: 1, color: '#999' })
 
 // render 10,000 hexes
@@ -17,13 +24,43 @@ grid = Grid.rectangle({ width: 50, height: 50 })
 grid.forEach(hex => {
     const { x, y } = hex.toPoint()
     // use hexSymbol and set its position for each hex
-    draw.use(hexSymbol).translate(x, y)
+    if(hex.custom == 'empty'){
+        draw.use(hexEmpty).translate(x, y)
+    } else if(hex.custom == 'wall'){
+        draw.use(hexWall).translate(x, y)
+    }
+    
+})
+
+var clicked = false
+
+document.addEventListener('mousedown', ({ offsetX, offsetY }) => {
+    clicked = true
+})
+
+document.addEventListener('mouseup', ({ offsetX, offsetY }) => {
+    clicked = false
+})
+
+document.addEventListener('mouseover', ({ offsetX, offsetY }) => {
+    if(clicked){
+        // convert point to hex (coordinates)
+        const hexCoordinates = Grid.pointToHex(offsetX, offsetY)
+        // get the actual hex from the grid
+        hex = grid.get(hexCoordinates)
+        const { x, y } = hex.toPoint()
+        if(hex.custom == 'empty'){
+            hex.custom = 'wall'
+            draw.use(hexWall).translate(x, y)
+        } else if(hex.custom == 'wall'){
+            hex.custom = 'empty'
+            draw.use(hexEmpty).translate(x, y)
+        }
+        
+        console.log(hex)
+    }
 })
 
 
-document.addEventListener('click', ({ offsetX, offsetY }) => {
-    // convert point to hex (coordinates)
-    const hexCoordinates = Grid.pointToHex(offsetX, offsetY)
-    // get the actual hex from the grid
-    console.log(grid.get(hexCoordinates))
-})
+
+
